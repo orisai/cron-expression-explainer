de5270f5fcdae86d633d8706db21003cc995cf27
Marek Bartoš <bartos.developer152@gmail.com>
2024-03-11 08:06:41 +0100

    CronExpressionExplainer: throws InvalidExpression

--- src/CronExpressionExplainer.php
+++ src/CronExpressionExplainer.php
@@ -3,12 +3,14 @@
 namespace Orisai\CronExpressionExplainer;
 
 use DateTimeZone;
+use Orisai\CronExpressionExplainer\Exception\InvalidExpression;
 
 interface CronExpressionExplainer
 {
 
 	/**
 	 * @param int<0,59>|null $repeatSeconds
+	 * @throws InvalidExpression
 	 */
 	public function explain(string $expression, ?int $repeatSeconds = null, ?DateTimeZone $timeZone = null): string;
 
